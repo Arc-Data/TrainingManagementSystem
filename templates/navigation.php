995e9cc59ab2f718b7c8f1461e444c8a0d4e59a1
ron-ligsay <akyligsay16@gmail.com>
2023-03-06 14:10:37 +0800

    fix navbar by elyza

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,43 +1,40 @@
-
 <nav class = "navbar navbar-expand-lg navbar-dark fixed-top pe-5 ps-5 h-18" style = "background-color: #681A1A;">
 
         <a class = "navbar-brand" href="../user/homepage.php" style="font-size:23px;" >Web Development</a>
         <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#portfolio-nav" aria-controls = "portfolio-nav" aria-expanded = "false" aria-label = "Toggle navigation">
             <span class = "navbar-toggler-icon"></span>
         </button>
-		
+
         <?php if((isset($_SESSION['user_type']))): ?>
         <div class = "collapse navbar-collapse" id = "portfolio-nav">
             <ul class="nav navbar-nav me-auto">
                 <li class ="nav-item">
                     <a class="nav-link" href="../user/homepage.php" style="font-size:15px;">HOME</a>
-                </li>
-                <li class = "nav-item">
-                    <a class="nav-link" href="../student/index.php" style="font-size:15px;">STUDENT</a>
-                </li>
-                <li class = "nav-item">
-                    <a class="nav-link" href="../course/index.php" style="font-size:15px;">COURSE</a>
-                </li>
-                <li class = " nav-item">
-                    <a class="nav-link" href="../instructor/index.php" style="font-size:15px;">INSTRUCTORS</a>
-                </li>
-            
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="../report/index.php" style="font-size:15px;" >GENERATE REPORT</a>
-                </li>
 
                 <?php endif;  ?>
 
                     <?php if((isset($_SESSION['username']) && $_SESSION['user_type'] == 'admin')): ?>
                     <!-- </li> -->
+                    <li class = "nav-item">
+                        <a class="nav-link" href="../student/index.php" style="font-size:15px;">STUDENT</a>
+                    </li>
+                    <li class = "nav-item">
+                        <a class="nav-link" href="../course/index.php" style="font-size:15px;">COURSE</a>
+                    </li>
+                    <li class = " nav-item">
+                        <a class="nav-link" href="../instructor/index.php" style="font-size:15px;">INSTRUCTORS</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="../report/index.php" style="font-size:15px;" >GENERATE REPORT</a>
+                    </li>
                     <li class ="nav-item">
                         <a class="nav-link" href="../user/Admin-Module.php" style="font-size:15px;">UPDATE DATABASE</a>
                     </li>
                     <?php endif;  ?>
                 <!-- </li> -->
-            
+
             </ul>
-            
+
 
 
 <!--     <div class="right-side"> -->
